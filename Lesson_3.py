e9e2687fc3e60e06c1d111691def81cef38c0719
merlion-local <75911434+merlion-local@users.noreply.github.com>
2023-07-03 22:47:21 +0400

    Update Lesson_3.py
    with python-dotenv

--- Lesson_3.py
+++ Lesson_3.py
@@ -1,13 +1,15 @@
 import smtplib
+import os
+
 
 friend_name = "Иван"
 website = "https://dvmn.org/referrals/ZrywWY269gDOBwt07LUO3z6R0RFWKe7vzGuzS3Rf/"
 my_name = "Алексей"
 
 letter = '''
-From:local.testting@yandex.ru
+From:ssdd7772006@mail.ru
 To:smockotnin@yandex.ru
-Subject: Важно!
+Subject: python
 Content-Type: text/plain; charset="UTF-8";
 
 
@@ -29,13 +31,9 @@ Content-Type: text/plain; charset="UTF-8";
 На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''
 
 formatted_letter = letter.format(friend_name, my_name, website, website, website)
-formatted_letter =  letter.encode("UTF-8")
-server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
-login = 'local.testting@yandex.ru'
-password = 'fijxbjhuohxnzasq'
+server = smtplib.SMTP_SSL('smtp.mail.ru:465')
+login = os.environ['SMTP_LOGIN']
+password = os.environ['SMTP_PASSWORD']
 server.login(login, password)
-server.sendmail('local.testting@yandex.ru', 'smockotnin@yandex.ru', formatted_letter)
+server.sendmail('ssdd7772006@mail.ru', 'smockotnin@yandex.ru', formatted_letter.encode("UTF-8"))
 server.quit()
-
-
-
